6efcd6e5c57bedba5ce5799cbc0655700cfc436c
Ran <razaanarjuna@gmail.com>
2026-01-28 04:07:57 +0700

    Add bird movement and input handling script
    Implemented basic movement logic for the bird using CharacterBody2D in bird.gd, including gravity, jump input, and rotation. Updated bird.tscn to reference the new script and added the Bird node to game.tscn as an instance of bird.tscn, along with a Camera2D node.

--- Script/bird.gd
+++ Script/bird.gd
@@ -1,1 +1,21 @@
+# Explain this, that evry gdscript code node, has this to show what node we currently code
 extends CharacterBody2D
+
+# Variable, the @, var/const, name then the after :
+@onready var sprite_2d: Sprite2D = $Sprite2D
+const jumpVelocity: int = -400
+
+# Func, this & _ready and other nesecesrry func
+func _physics_process(delta: float) -> void:
+	# Get gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Accepting input, and tell the difference between action
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = jumpVelocity
+	
+	rotation_degrees = velocity.y * 0.1
+
+# Necesearry for movement i think
+	move_and_slide()
